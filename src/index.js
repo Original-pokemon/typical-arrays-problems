bcbfba8cde6b2d230ad2c235d8df73ad3f5143aa
original-pokemon <eremkin-02@mail.ru>
2021-03-11 08:17:48 +0300

    feat: add avg

--- src/index.js
+++ src/index.js
@@ -15,5 +15,8 @@ exports.max = function max (array) {
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!array || array.length === 0) {
+    return 0
+  }
+  return array.reduce((a, b) => a + b) / array.length;
 }
