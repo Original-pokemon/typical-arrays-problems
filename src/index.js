78c29b3ab03699144e87e45830efbb0454c47096
original-pokemon <eremkin-02@mail.ru>
2021-03-11 07:43:14 +0300

    feat: add min

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
+"use strict"
 
 exports.min = function min (array) {
-  return 0;
+  if (!array || array.length === 0) {
+    return 0
+  }
+  return Math.min(...array);
 }
 
 exports.max = function max (array) {
-  return 0;
+  return 0
 }
 
 exports.avg = function avg (array) {
