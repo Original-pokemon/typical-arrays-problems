9a5aa2d80e619c0eee5edc60a50a590e565f1cdd
original-pokemon <eremkin-02@mail.ru>
2021-03-11 07:43:45 +0300

    feat: add max

--- src/index.js
+++ src/index.js
@@ -8,7 +8,10 @@ exports.min = function min (array) {
 }
 
 exports.max = function max (array) {
-  return 0
+  if (!array || array.length === 0) {
+    return 0
+  }
+  return Math.max(...array);
 }
 
 exports.avg = function avg (array) {
